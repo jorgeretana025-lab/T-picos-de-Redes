e0c7f46e7598433186bac5b751441f877a0f8440
jorgeretana025-lab <jorgeretana025@gmail.com>
2026-03-23 16:36:20 -0600

    feat: :art: practica1_trafico_de_datos

--- Practica.py
+++ Practica.py
@@ -1,58 +1,79 @@
 import numpy as np
-import pandas as pd 
+import pandas as pd
 import matplotlib.pyplot as plt
 import random
 import time
 
 """
-Configuración
+Configuracion
 
 """
-Num_Paquetes = 100
-Tamano_Paquete = 1024 bytes
-veloicidad_red = 100000 
+Num_paquetes = 100
+Tamano_paquete = 1024 #bytes
+velocidad_red = 100000 #bytes por segundo
 
 """
-Listas para almanecar datos
-"""
+Listas para almacenar datos
 
+"""
 latencias = []
-paquetas_enviadas = []
+paquetes_envidas = []
 paquetes_recibidos = []
 perdidos = 0
 
-print("Simulando tráfico de datos...\ ")
+print("Simulando tráfico de datos . . .\n")
 
-for i in range(Num_Paquetes): 
+for i in range(Num_paquetes):
     tiempo_envio = time.time()
 
     """
-    Simular latencia de (entre 10ns y 100ns)
+    Simular latencia (entre 10ns y 100ns)
+
     """
-    latencia = random.uniform (0.01, 0.1)
+    latencia = random.uniform(0.01,0.1)
     time.sleep(latencia)
 
     """
-    Simular perida de paquetes (10%)
+    Simular perdidas de paquetes (10%)
     """
     if random.random () < 0.1:
-        perdidos.random () < 0.1:
         perdidos += 1
         continue
 
     tiempo_recepcion = time.time()
-    latencia.appende(tiempo_recepcion - tiempo_recepcion)
-    paquetas_enviadas.append(Tamano_Paquete)
-    paquetes_recibidos.append(Tamano_Paquete)
+    latencias.append(tiempo_recepcion - tiempo_envio)
+    paquetes_envidas.append(Tamano_paquete)
+    paquetes_recibidos.append(Tamano_paquete)
 
     """
     Metricas
     """
-    total_enviados = len(paquetas_enviadas)
-    total_recibidos = len(paquetes_recibidos)
-    tasa_perdida = perdidos / Num_Paquetes
-    
-    latencia_promedio = np.mean(latencias)
-    
-    throughput = (sum(paquetes_recibidos) / sub (latencias)) if latencias else 0
-    
+total_enviados = len(paquetes_envidas)
+total_recibidos = len(paquetes_recibidos)
+tasa_perdida = perdidos / Num_paquetes
+
+latencia_promedio = np.mean(latencias)
+# Througtput (bytes/segundo)
+throughput = (sum(paquetes_recibidos)/sum (latencias)) if latencias else 0
+
+"""
+Configutación de Resultados
+"""
+
+print("Paquetes envidas: {total_enviados}")
+print("Paquetes recibidos: {total_recibidos}")
+print("Paquetes perdidos: {perdidos}")
+print("Tasa de perdidad: {tasa_perdida:.2f}")
+print("Tasa de perdidad: {latencia_promedio:.2f}")
+print("Througtput: {Througtput:.2f} bytes/s ")
+
+"""
+Grafica de latencias
+"""
+
+plt.plot(latencias)
+plt.title("Latencia por paquete")
+plt.xlabel("paquetes")
+plt.ylabel("Latencia(s)")
+plt.grid()
+plt.show()
